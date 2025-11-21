b9d6526c7444b81993790529c17012dbb6130739
EstGui <guilherme.o.esteves@gmail.com>
2025-11-20 22:38:36 -0300

    cors update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import router from './routes';
+import cors from "cors";
 
 
 function CreateApp() {
     const app = express();
     
+    app.use(cors({
+        origin: "*", // seu frontend
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true, // se precisar enviar cookies
+    }))
     app.use(express.json());
     app.use("/api", router)
 
